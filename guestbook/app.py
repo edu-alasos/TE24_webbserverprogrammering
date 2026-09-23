cb69a25de0582676d80e4243d1f5e7e0b450dc77
edu-alasos <alansebastian.soszynski@skola.taby.se>
2026-09-23 14:36:25 +0200

    hantera requesten

--- guestbook/app.py
+++ guestbook/app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-if __name__ == '__main__':
+
+@app.route("/send", methods=["POST"])
+def send():
+    print(request.form)
+    name = request.form.get("name")
+    mail = request.form.get("mail")
+    homepage = request.form.get("homepage")
+    phone = request.form.get("phone")
+    comment = request.form.get("comment")
+
+    full = {"name": name,
+            "mail": mail,
+            "homepage": homepage,
+            "phone": phone,
+            "comment": comment}
+    
+    print(full)
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
